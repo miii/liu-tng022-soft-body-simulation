158d30f4ca5e6a6f6623d7476dfb1278ffa01fc3
Jacob Andersson <jacob.m.andersson@gmail.com>
2017-02-17 14:06:46 +0100

    Test variables changed

--- preparation/nMassesEuler2D/nMassesEuler2D.m
+++ preparation/nMassesEuler2D/nMassesEuler2D.m
@@ -3,20 +3,21 @@ clear all
 close all
 
 m = 2; % mass 
-k = 20; % spring constant
-d = 5; % damper constant
-r = 0.5; % rest length
+k = 50; % spring constant
+d = 20; % damper constant
+r = 0.8; % rest length
 
 h = 0.01; % step size
-samples = 20 / h;
+samples = 10 / h;
 
-n = 3; % number of masses
 % initial state
-x(1,1:n) = [-1 0 1];   
-vx(1,1:n) = [0 0 0];
+x(1,:) = [-1 0 1];   
+vx(1,:) = [0 0 0];
 
-y(1,1:n) = [0 1 0];   
-vy(1,1:n) = [0 0 0];
+y(1,:) = [0 1 0];   
+vy(1,:) = [0 0 0];
+
+n = length(x);
 
 [xa, vxx] = create_MSD(x, vx, samples, n, m, k, d, r, h);
 [ya, vyy] = create_MSD(y, vy, samples, n, m, k, d, r, h);
@@ -24,7 +25,11 @@ vy(1,1:n) = [0 0 0];
 %figure(1);
 %plot(xa)
 
-%figure(2);
-%plot(ya)
+% figure(2);
+% plot(ya(:, 1:2))
+% figure(3);
+% plot(ya(:, 2:3))
+% figure(4)
+% plot(ya)
 
 visualize2D(xa, ya)
